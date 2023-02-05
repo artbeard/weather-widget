0faa6ca19a2ea18269e76ce581756b1a09309cbd
artbeard <frolov.web@gmail.com>
2023-02-05 22:54:19 +0600

    Add output of weather in cities from the list

--- src/components/WeatherBoard.vue
+++ src/components/WeatherBoard.vue
@@ -1,12 +1,36 @@
 <template>
-	<div>
-		Блок отображения гоордов с погодой	
-	</div>
+	<Panel class="weater-no-border weather-mb weather-mb-content" v-for="(city, index) in cities" :key="city">
+		<template #header>
+			<b>{{city}}</b>
+		</template>
+		<template #icons v-if="index == 0">
+			<button class="p-panel-header-icon p-link mr-2" @click="toggleToConfig">
+				<SvgIcon icon="gear" />
+			</button>
+		</template>
+		<WeatherCity />
+	</Panel>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import WeatherCity from './WeatherCity.vue';
 export default defineComponent({
-	name: 'WeatherBoard',	
+	name: 'WeatherBoard',
+	components: {
+		WeatherCity,
+	},
+	data: ()=>({
+		cities: [
+			'London',
+			'Moscow'
+		]
+	}),
+	methods:{
+		toggleToConfig()
+		{
+			console.log('Переключаем на конфигурацию');
+		}
+	}
 })
 </script>
