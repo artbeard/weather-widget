50152f2eb4bb6186ac4f30e3a440b57f339c1e84
artbeard <frolov.web@gmail.com>
2023-02-05 22:53:20 +0600

    Add markup for configuration panel

--- src/components/WeatherConfig.vue
+++ src/components/WeatherConfig.vue
@@ -1,12 +1,84 @@
 <template>
-	<div>
-		Блок отображения настройки	
-	</div>
+	<Panel>
+		<template #header>
+			Widget Configure
+		</template>
+		
+		<template #icons>
+			<button class="p-panel-header-icon p-link mr-2" _click="toggle">
+				<SvgIcon icon="check-lg" />
+			</button>
+			<button class="p-panel-header-icon p-link mr-2" _click="toggle">
+				<SvgIcon icon="x-lg" />
+			</button>
+		</template>
+
+		<div class="weater-city-list">
+			<div class="weater-city-list__item">
+				<span class="weater-city-list__item-sort-handler">
+					<SvgIcon icon="list" />
+				</span>
+				<span class="weater-city-list__item-city-label">Город1</span>
+				<Button label="Primary" class="p-button-danger p-button-sm p-button-text weater-city-list__item-controll-del"><SvgIcon icon="trash" /></Button>
+			</div>
+			<div class="weater-city-list__item">
+				<span class="weater-city-list__item-sort-handler">
+					<SvgIcon icon="list" />
+				</span>
+				<span class="weater-city-list__item-city-label">Город1</span>
+				<Button label="Primary" class="p-button-secondary p-button-sm p-button-text weater-city-list__item-controll-del"><SvgIcon icon="trash" /></Button>
+			</div>
+			<div class="weater-city-list__item">
+				<span class="weater-city-list__item-sort-handler">
+					<SvgIcon icon="list" />
+				</span>
+				<span class="weater-city-list__item-city-label">Город1</span>
+				<Button label="Primary" class="p-button-secondary p-button-sm p-button-text weater-city-list__item-controll-del"><SvgIcon icon="trash" /></Button>
+			</div>
+		</div>
+		
+		<div class="p-inputgroup">
+			<AutoComplete
+				v-model="selectedCountry"
+				_suggestions="filteredCountriesBasic"
+				@complete="searchCountry($event)" 
+				optionLabel="--" placeholder="SearchCountry">
+				<template #loader>
+					<div class="weather-spinner"></div>			
+				</template>
+			</AutoComplete>
+		</div>
+		
+			
+	</Panel>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
+import InputText from 'primevue/inputtext';
+import AutoComplete from 'primevue/autocomplete';
 export default defineComponent({
-	name: 'WeatherConfig',	
-})
+	name: "WeatherConfig",
+	components: {
+		InputText,
+		AutoComplete
+	},
+
+	data() {
+		return {
+			selectedCountry: null,
+			filteredCountries: null
+		}
+	},
+	countryService: null,
+	created() {
+		//this.countryService = new CountryService();
+	},
+	methods: {
+		searchCountry(event: any) {
+            //this.filteredCountriesBasic = this.countryService.search(event.query);
+		}
+	}
+
+});
 </script>
