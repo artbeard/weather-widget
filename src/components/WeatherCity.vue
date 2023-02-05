d610ea0f2b7f2486a1a458198a1968ed507bce5f
artbeard <frolov.web@gmail.com>
2023-02-05 22:52:28 +0600

    add markup for city weather card

--- src/components/WeatherCity.vue
+++ src/components/WeatherCity.vue
@@ -1,12 +1,25 @@
 <template>
-	<div>
-		Блок отображения погоды в одном городе	
+	<div class="weater-city-block">
+		<div class="weater-city-block__condition">
+			<div class="weater-city-block__condition-img">
+				<img src="http://openweathermap.org/img/wn/10d@2x.png" alt="" />
+			</div>
+			<div class="weater-city-block__condition-temperaure">+36 C</div>
+		</div>
+		<p class="weater-city-block__description">Description Weather</p>
+		<div class="weater-city-block__forecast">
+			<div class="weater-city-block__forecast-item">Давление</div>
+			<div class="weater-city-block__forecast-item">Влажность</div>
+			<div class="weater-city-block__forecast-item">Ветер</div>
+			<div class="weater-city-block__forecast-item">Точка россы</div>
+			<div class="weater-city-block__forecast-item">хз</div>
+		</div>
 	</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 export default defineComponent({
-	name: 'WeatherCity',	
-})
+	name: "WeatherCity",
+});
 </script>
