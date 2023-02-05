9ceef7d034fca082fbe34cb7bfca6a3909e74128
artbeard <frolov.web@gmail.com>
2023-02-05 16:38:35 +0600

    add component for displaying the list of weather in cities

---
+++ src/components/WeatherBoard.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>
+		Блок отображения гоордов с погодой	
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+	name: 'WeatherBoard',	
+})
+</script>
