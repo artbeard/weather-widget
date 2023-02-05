47f9cee648ffd047daae4a4c6802881d65bca873
artbeard <frolov.web@gmail.com>
2023-02-05 22:55:01 +0600

    registering icons and components globally

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import SvgIcon from './components/SvgIcon.vue';
 import PrimeVue from 'primevue/config';
+import Panel from "primevue/panel";
+import Button from "primevue/button";
 
 createApp(App)
 	.use(PrimeVue)
+	.component('SvgIcon', SvgIcon)
+	.component('Panel', Panel)
+	.component('Button', Button)
 	.mount('#app')
