6c41b18a146d7da164772a9cf8acf982d682e33b
artbeard <frolov.web@gmail.com>
2023-02-05 17:02:49 +0600

    import styles primevue

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,24 @@
 </template>
 
 <script lang="ts">
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
 import { defineComponent } from "vue";
 import WeatherConfig from "./components/WeatherConfig.vue";
+import Button from 'primevue/button';
 export default defineComponent({
 	name: "App",
 	components: {
-		WeatherConfig
+		WeatherConfig,
+		Button
 	},
 });
 </script>
 
 <style lang="scss" scoped>
 .weather-widget {
-	font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+	//font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+	line-height: 1.3em;
 	min-width: 300px;
 	max-width: 350px;
 	padding: 1em;
