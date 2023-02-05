739b7cbccdd1c98dd11ecb120864edb2b6beea90
artbeard <frolov.web@gmail.com>
2023-02-05 16:37:53 +0600

    add component for displaying weather in one city

---
+++ src/components/WeatherCity.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>
+		Блок отображения погоды в одном городе	
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+	name: 'WeatherCity',	
+})
+</script>
