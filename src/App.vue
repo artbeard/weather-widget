375a5a5c3d9631f0003431e5121871db31372c05
artbeard <frolov.web@gmail.com>
2023-02-05 16:47:20 +0600

    Add basic component style

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
-<template></template>
+<template>
+	<div class="weather-widget">
+		<WeatherConfig />
+	</div>
+</template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-
+import WeatherConfig from "./components/WeatherConfig.vue";
 export default defineComponent({
 	name: "App",
-	components: {},
+	components: {
+		WeatherConfig
+	},
 });
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+.weather-widget {
+	font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+	min-width: 300px;
+	max-width: 350px;
+	padding: 1em;
+}
 </style>
