7090a13b7bb55b8af16ca4753a328f4ec87ec1fa
artbeard <76746834+artbeard@users.noreply.github.com>
2023-02-06 17:38:47 +0600

    Making api endpoints into a one module

---
+++ src/use/urlsapi.ts
@@ -0,0 +1,2 @@
+export const searchLocation: string = `https://api.openweathermap.org/geo/1.0/direct?q=:query&limit=5&appid=:apiKey`;
+export const getWeatherData: string = `https://api.openweathermap.org/data/2.5/weather?lat=:lat&lon=:lon&appid=:apiKey&units=metric&lang=:locale`;
