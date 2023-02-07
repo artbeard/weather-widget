cb9ce93d66bb1cef71a2cd993819d1fa58d3f7e4
artbeard <frolov.web@gmail.com>
2023-02-07 11:37:59 +0600

    Transferring Application Creation to a Web Component

--- src/WeatherWidget.ts
+++ src/WeatherWidget.ts
@@ -1,5 +1,9 @@
 import { createApp, App as Application } from 'vue'
 import App from './App.vue'
+import SvgIcon from './components/SvgIcon.vue';
+import PrimeVue from 'primevue/config';
+import Panel from "primevue/panel";
+import Button from "primevue/button";
 
 export default class WeatherWidget extends HTMLElement {
 
@@ -10,6 +14,10 @@ export default class WeatherWidget extends HTMLElement {
 	 */
 	private vueFactory(): Application {
 		const app = createApp(App);
+		app.use(PrimeVue);
+		app.component('SvgIcon', SvgIcon);
+		app.component('Panel', Panel);
+		app.component('Button', Button);
 		return app;
 	}
 
@@ -22,6 +30,7 @@ export default class WeatherWidget extends HTMLElement {
 	 */
 	connectedCallback() {
 		this.App = this.vueFactory();
+		this.App.provide('apiKey', this.dataset?.apiKey ?? null)
 		this.App.mount(this);
 	}
 
