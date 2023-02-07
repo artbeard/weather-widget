c4236fbea91b49ee49d4e9bee6dd916b1ecc4013
artbeard <frolov.web@gmail.com>
2023-02-07 09:27:28 +0600

    Configuring webpack to build into a single file

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-	transpileDependencies: true
+	transpileDependencies: true,
+	productionSourceMap: false,
+	filenameHashing: false,
+	integrity: true,
+	css:{
+		extract: false
+	},
+	configureWebpack: {
+		optimization: {
+		  splitChunks: false,
+		},
+	},
 })
