b7236abad33941470af5215e7741ce3a0c0c8feb
artbeard <frolov.web@gmail.com>
2023-02-07 12:19:37 +0600

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
 	transpileDependencies: true,
 	/*
+	//раскоментировать для сборки в один js файл
 	productionSourceMap: false,
 	filenameHashing: false,
 	integrity: true,
