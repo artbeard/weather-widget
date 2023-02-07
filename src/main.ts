89e0f3c5ba8c819e464928e709b17f73f69224b8
artbeard <frolov.web@gmail.com>
2023-02-07 11:38:49 +0600

    Registering a Web Component

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,3 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import SvgIcon from './components/SvgIcon.vue';
-import PrimeVue from 'primevue/config';
-import Panel from "primevue/panel";
-import Button from "primevue/button";
-
-createApp(App)
-	.use(PrimeVue)
-	.component('SvgIcon', SvgIcon)
-	.component('Panel', Panel)
-	.component('Button', Button)
-	.provide('apiKey', '6b4eea3a03b7f4c09a93f8cb0c9ded58') //apiKey для отладки
-	.mount('#app')
+import WeatherWidget from './WeatherWidget'
+customElements.define("weather-widget", WeatherWidget);
